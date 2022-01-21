604778c7ecaa2456d8d69eae70b4e2df9156b3f2
Matthew Setter <matthew@matthewsetter.com>
2022-01-21 13:16:33 +0100

    Add ability to differentiate between environments
    This change does so by using PHP Dotenv, which sets a MODE variable
    which the code can read, to determine which mode its in. What it also
    does is to set the posts data file directory based on the environment as
    well, which makes the application testable, as the test files can be set
    and not be subject to change.

--- public/index.php
+++ public/index.php
@@ -15,12 +15,16 @@ use Slim\Views\{Twig,TwigMiddleware};
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../', ['.env', '.env.development'], false);
+$dotenv->safeLoad();
+$dotenv->required('POSTS_DIRECTORY');
+
 $container = new Container();
 $container->set('view', fn() => Twig::create(__DIR__ . '/../resources/templates'));
 $container->set(
     ContentAggregatorInterface::class,
     fn() => (new ContentAggregatorFactory())->__invoke([
-        'path' => __DIR__ . '/../data/posts',
+        'path' => __DIR__ . sprintf('/../%s', $_SERVER['POSTS_DIRECTORY']),
         'parser' => new Parser(),
     ])
 );
